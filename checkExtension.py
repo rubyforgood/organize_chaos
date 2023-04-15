12ad923069aed0979a7fc4672632824bf0cc3843
ramosv <56368263+ramosv@users.noreply.github.com>
2023-04-15 13:50:50 -0600

    File types

--- checkExtension.py
+++ checkExtension.py
@@ -2,14 +2,22 @@ import sys
 import os
 from os import listdir
 from os.path import isfile, join
+import json
+import pandas
 
 
+def readDir():
+    with open('./Resources/lexicon.json', 'r') as j:
+        lex = json.loads(j.read())
+    return list(lex.keys())
+    
 
 def reader():
-    [_,dir] = sys.argv
-    extensions = {}
-    readerFiles(dir, extensions)
-    print(extensions)   
+    [] = sys.argv
+    #[_,dir] = sys.argv
+    #extensions = {}
+    #readerFiles(dir, extensions)
+    #print(extensions)   
 
 def readerFiles(path, extensions):
              
@@ -24,5 +32,17 @@ def readerFiles(path, extensions):
             dir = f"{path}/{f}"
             readerFiles(dir,extensions)
 
+def checkForLex(path):
+    lex = readDir()
+    mainList = []
+    for f in listdir(path):
+        for word in lex:
+            if word in lex:
+                mainList.append(())
+                
+    
+
 if __name__ == "__main__":
-    reader()
+    #reader()
+    path = "C:/Users/ramosv/Desktop/MileHack - The Fax Denver"
+    checkForLex(path)
