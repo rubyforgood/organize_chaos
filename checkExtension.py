3fc63821115171c2bca23bd2a65b1bf5a897ab9b
ramosv <56368263+ramosv@users.noreply.github.com>
2023-04-15 14:27:31 -0600

    files

--- checkExtension.py
+++ checkExtension.py
@@ -3,13 +3,13 @@ import os
 from os import listdir
 from os.path import isfile, join
 import json
-import pandas
+import pandas as pd
 
 
 def readDir():
     with open('./Resources/lexicon.json', 'r') as j:
         lex = json.loads(j.read())
-    return list(lex.keys())
+    return lex
     
 
 def reader():
@@ -19,28 +19,32 @@ def reader():
     #readerFiles(dir, extensions)
     #print(extensions)   
 
-def readerFiles(path, extensions):
+# def readerFiles(path, extensions):
              
-    for f in listdir(path):
-        if isfile(join(path, f)):
-            extension = f.split(".").pop()
-            if not extension in extensions:
-                extensions[extension] = 1
-            else:
-                extensions[extension] +=1
-        else:
-            dir = f"{path}/{f}"
-            readerFiles(dir,extensions)
+#     for f in listdir(path):
+#         if isfile(join(path, f)):
+#             extension = f.split(".").pop()
+#             if not extension in extensions:
+#                 extensions[extension] = 1
+#             else:
+#                 extensions[extension] +=1
+#         else:
+#             dir = f"{path}/{f}"
+#             readerFiles(dir,extensions)
 
 def checkForLex(path):
     lex = readDir()
     mainList = []
     for f in listdir(path):
         for word in lex:
-            if word in lex:
-                mainList.append(())
+            if word.lower() in f.lower():
+                mainList.append([f,word])
+            else:
+                continue
+    df = pd.DataFrame(mainList, columns=["File name", "Word"])
+    breakpoint()
+    return df
                 
-    
 
 if __name__ == "__main__":
     #reader()
