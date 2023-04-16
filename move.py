3734e812621f269eb56678fdac8655192993b549
eddie-semarts <eddie.thomas@semanticarts.com>
2023-04-16 13:23:20 -0600

    Update for moving

--- move.py
+++ move.py
@@ -1,5 +1,17 @@
 import json
+import sys
 import subprocess
+
+[_, FINAL_OUTPUT] = sys.argv
+
+SHORT_NAME_TO_FILE_NAME = {
+    "motel": "Motel",
+    "bso": "Business Support Office",
+    "edo": "Equity Development Office",
+    "rso": "Resident Support Office",
+    "comm": "Communication",
+    "admin": "Admin",
+}
 
 
 # Test JSON
@@ -28,48 +40,53 @@ import subprocess
 # `cp` command until positive this code does what you want.
 
 
-# This code can also move to the file that would right the JSON and then
-# we can just call the for loop on the array of sorted files
-_ = "./test.json"
-
-with open(_, "r") as sorted_file_json:
+with open(FINAL_OUTPUT, "r") as sorted_files:
     # Load JSON and get array of dictionaries
-    sorted_file_dicts = json.loads(sorted_file_json.read())
-    # Loop on array of dictionaries
-    for file_dict in sorted_file_dicts:
+    sorted_file_array = sorted_files.readlines()
+    # Loop on array of arrays
+    for line in sorted_file_array:
+        if (sorted_file_array[0]) == line:
+            continue
+
+        [index, file, folder] = line.split(",")
         # Get the file and category
-        file = file_dict["file"]
+        category = SHORT_NAME_TO_FILE_NAME[folder]
+
+        #  --- DEPRECATED  --------------
+
         # Get the matches
-        category_matches = file_dict["matches"]
+        # category_matches = file_dict["matches"]
         # max matches
-        max_matches = max(category_matches.values())
+        # max_matches = max(category_matches.values())
         # Filter all the match objects in the file's matches, in most cases we
         # should only have one thing have the most matches, but if we have multiple
         # we'll need user intervention
-        category: list = list(
-            # Filter
-            filter(
-                # Inline function for checking if the category has the max number of matches
-                lambda cat_key: category_matches[cat_key] == max_matches,
-                # Pass the keys of the match dict to filter on
-                category_matches.keys(),
-            )
-        )
+        # --------------
+        # category: list = list(
+        #     # Filter
+        #     filter(
+        #         # Inline function for checking if the category has the max number of matches
+        #         lambda cat_key: category_matches[cat_key] == max_matches,
+        #         # Pass the keys of the match dict to filter on
+        #         category_matches.keys(),
+        #     )
+        # )
 
         # If the length is bigger than 1, then allow a user to look at the file name and put in a guess
         # Also let the user know how many categories have the max number of matches
-        if len(category) > 1:
-            category: str = category[
-                int(
-                    input(
-                        f"\n\nThe file in question: `{file}`\n\nIt has multiple categories: {category}\n\nEnter the index of the category: "
-                    )
-                )
-            ]
-        else:
-            category: str = category.pop()
+        # --------------
+        # if len(category) > 1:
+        #     category: str = category[
+        #         int(
+        #             input(
+        #                 f"\n\nThe file in question: `{file}`\n\nIt has multiple categories: {category}\n\nEnter the index of the category: "
+        #             )
+        #         )
+        #     ]
+        # else:
+        #     category: str = category.pop()
 
         # Assign where the file will be copies to
-        moved_dir = f"./Testing/{category}"
+        moved_dir = f"./GoogleDrive/{category}"
         # Run the subprocess to move it.
         subprocess.run(["cp", file, moved_dir])
